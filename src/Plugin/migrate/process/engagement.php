df9d67b2f80c4940b96eef517f367d07cf21b162
Douglas Tschetter <me@douglast.com>
2018-10-08 22:15:51 -0500

    Adding engagement migration

--- src/Plugin/migrate/process/engagement.php
+++ src/Plugin/migrate/process/engagement.php
@@ -5,6 +5,7 @@ namespace Drupal\ercore_migrate\Plugin\migrate\process;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\Core\Database\Database;
 
 /**
  * Date processing, changes time code from D7 date.
@@ -29,8 +30,24 @@ class Engagement extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    drush_print($value);
-    return $value;
+    return self::getId($value);
+  }
+
+  /**
+   * Receives Node Id, returns Node ID of referencing Engagement.
+   *
+   * @param int $nid
+   *   Receives Node ID of original node.
+   *
+   * @return int
+   *   Returns integer ID of related node.
+   */
+  public function getId($nid) {
+    $query = Database::getConnection('default', 'migration')
+      ->select('field_data_field_er_cal_entity_reference', 'e')
+      ->fields('e', ['field_er_cal_entity_reference_target_id'])
+      ->condition('e.entity_id', $nid);
+    return $query->execute()->fetchField();
   }
 
 }
